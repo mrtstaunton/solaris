50cbc8647aba8e4265e3a1028cb4b0febdb1ad6f
Tom Staunton <me@tomstaunton.com>
2017-11-23 16:03:42 +0000

    Create f20-card-tweak.sh

---
+++ f20-card-tweak.sh
@@ -0,0 +1,2 @@
+echo 'ssd-config-list = "ATA     MARVELL SD88SA02","throttle-max:32, disksort:false, cache-non:true";' >> /kernel/drv/ssd.conf
+echo 'ssd-config-list = "ATA     MARVELL SD88SA02","throttle-max:32, disksort:false, cache-non:true";' >> /kernel/drv/sd.conf
